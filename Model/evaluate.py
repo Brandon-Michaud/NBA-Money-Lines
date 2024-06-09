87b673faab05e4ff80a548cfde55d3b946973101
Brandon Michaud <michaudbrandond@gmail.com>
2024-06-09 15:50:33 -0500

    Added code to calculate baseline performance

--- Model/evaluate.py
+++ Model/evaluate.py
@@ -1,6 +1,6 @@
 import pickle
 import numpy as np
-from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from data import *
 import scipy
 import matplotlib.pyplot as plt
@@ -49,6 +49,14 @@ def perform_paired_ttest(sample1, sample2, confidence=0.95):
         print(f'We are NOT at least {confidence * 100}% confident that the samples come from different distributions')
 
 
+def find_baseline_prediction(real_values):
+    avg_values = np.mean(real_values, axis=0)
+    avg_values_arr = np.full(real_values.shape, avg_values)
+    baseline_mae = mean_absolute_error(real_values, avg_values_arr)
+    baseline_mse = mean_squared_error(real_values, avg_values_arr)
+    return baseline_mse, baseline_mae
+
+
 def make_residual_plot_total(results):
     test_real = results['test_real']
     test_pred = results['test_pred']
@@ -94,7 +102,8 @@ if __name__ == '__main__':
     intermediate_2_cv_results = load_cross_validation_results('../Results/intermediate_2_rotation_{}_layers_256_128_64_act_elu_lrate_1e-05_results.pkl', rotations)
     intermediate_2_cv_mses, intermediate_2_cv_maes, intermediate_2_cv_r2s = extract_metrics(intermediate_2_cv_results)
 
-    perform_paired_ttest(intermediate_cv_maes, intermediate_2_cv_maes, confidence=0.95)
+    # perform_paired_ttest(intermediate_cv_maes, intermediate_2_cv_maes, confidence=0.95)
+    find_baseline_prediction(intermediate_2_cv_results[0]['test_real'])
 
     # make_residual_plot_spread(intermediate_2_cv_results[0])
 
@@ -103,20 +112,3 @@ if __name__ == '__main__':
     #
     # simple_players_results = load_cross_validation_results('../Results/simple_players_rotation_{}_256_128_64_act_elu_lrate_1e-05_results.pkl', rotations)
     # simple_players_maes = extract_maes(simple_players_results)
-
-    # results = load_results('../Results/moderate_model_results.pkl')
-    # x_test = results['x_test']
-    # y_test_pred = results['y_test_pred']
-    # y_test_real = results['y_test_real']
-    #
-    # home_team_score = y_test_real[:, 0]
-    # away_team_score = y_test_real[:, 1]
-    # avg_home_team_score = np.mean(home_team_score)
-    # avg_away_team_score = np.mean(away_team_score)
-    # avg_home_team_score_arr = np.full(home_team_score.shape, avg_home_team_score)
-    # avg_away_team_score_arr = np.full(away_team_score.shape, avg_away_team_score)
-    # home_baseline_mae = mean_absolute_error(home_team_score, avg_home_team_score_arr)
-    # away_baseline_mae = mean_absolute_error(away_team_score, avg_away_team_score_arr)
-    # baseline_mae = (home_baseline_mae + away_baseline_mae) / 2
-    # print(f'Actual MAE: {results["test_eval"][1]}')
-    # print(f'Baseline MAE: {baseline_mae}')
