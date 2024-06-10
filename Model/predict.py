71a8fe4c13403af398caadc66f3a97dda5eedee0
Brandon Michaud <michaudbrandond@gmail.com>
2024-06-10 01:15:15 -0500

    Added code to find average prediction

--- Model/predict.py
+++ Model/predict.py
@@ -6,11 +6,22 @@ def load_saved_model(model_path):
     return load_model(model_path)
 
 
+def load_saved_models(model_path, rotations):
+    models = []
+    for rotation in rotations:
+        models.append(load_saved_model(model_path.format(rotation)))
+    return models
+
+
+def get_average_prediction(inputs, outputs, models):
+    predictions = np.empty((len(models), *outputs.shape))
+    for i, model in enumerate(models):
+        predictions[i] = model.predict(inputs, verbose=0)
+    avg_predictions = np.mean(predictions, axis=0)
+    return avg_predictions
+
+
 if __name__ == '__main__':
-    model = load_saved_model('../Results/intermediate_layers_256_128_64_act_elu_lrate_1e-05_model.keras')
-    inputs, outputs = load_dataset('../Datasets/predictions_2024-06-06.pkl')
-    num_features = inputs.shape[1]
-    for x in inputs:
-        x = x.reshape(1, num_features)
-        predictions = model.predict(x)
-        print(predictions)
+    models = load_saved_models('../Results/intermediate_rotation_{}_layers_256_128_64_act_elu_lrate_1e-05_model.keras', range(10))
+    inputs, outputs = load_dataset('../Datasets/predictions_2024-06-09.pkl')
+    print(get_average_prediction(inputs, outputs, models))\
